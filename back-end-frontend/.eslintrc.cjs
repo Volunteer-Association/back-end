b94e693f476db46af352c8db3e40a7f2d0e607d7
chenguihui <2404296720@qq.com>
2024-08-26 00:40:45 +0800

    项目架构重组

--- back-end-frontend/.eslintrc.cjs
+++ back-end-frontend/.eslintrc.cjs
@@ -6,17 +6,10 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting',
-    // "plugin:vue/vue3-recommended"
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  },
-  rules: {
-    "vue/multi-word-component-names": ["error", {
-      "ignores": [
-        "Login", "Example"
-      ]
-    }]
   }
 }
--- back-end-frontend/src/router/index.js
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Routes from './router';
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: Routes
-})
-
-export default router
