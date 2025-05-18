44a1379d5061b65538d1352bd593d82b2f33294c
chenguihui <2404296720@qq.com>
2025-05-18 18:04:55 +0800

    版本修改

--- back-end-backend/src/auth/register/register.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRegisterDto } from './dto/create-register.dto';
-
-@Injectable()
-export class RegisterService {
-  create(createRegisterDto: CreateRegisterDto) {
-    return 'This action adds a new register';
-  }
-}
